908ba240aa344deca3c9f4dda09c281466eb9fd2
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-02-11 13:15:50 +0530

    Created user model and auth apis

---
+++ server/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 30,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,        
+    },    
+}, {
+    timestamps: true,
+})
+
+const userModel = mongoose.model("User", userSchema)
+
+module.exports = userModel
